0282f4fb034f51e74dec808021e0fa7063bef5f0
Marcelo <marcelo.postiglione@gmail.com>
2025-09-27 23:24:12 -0300

    feat: Text extraction from files. Support to eml files (text files with structured email information)

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,11 @@
+# Lib imports
 from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+from pypdf import PdfReader
 
+import chardet
+
+# Local Imports
 from utils.files import *
 
 # Configuring static folder to serve the frontend files
@@ -24,16 +29,32 @@ def verify_email():
             print("Arquivo válido recebido!")
             
             # Handling the file types
-            if (uploaded_file.mimetype == "application/octet-stream"):  # Text files
+            if (uploaded_file.mimetype == "text/plain" or uploaded_file.mimetype == "message/rfc822"):  # Text files
                 print("text file")
+                # Read all contents
+                raw_content = uploaded_file.stream.read()
+                encoding = chardet.detect(raw_content)["encoding"]
+                print(f"Encoding {encoding}")
+                text = raw_content.decode(encoding)
             
             elif (uploaded_file.mimetype == "application/pdf"):
                 print("PDF file")
+                reader = PdfReader(uploaded_file)
+                text = ""
+                for page_number in range(len(reader.pages)):
+                    page = reader.pages[page_number]
+                    text += page.extract_text()
 
         else:
             return jsonify({
                 "error": "Invalid file extension"
             }), 400
+
+        return jsonify({
+            "response": msg,
+            "status": 200,
+            "file_text_text": text
+        }), 200
 
     elif request.form:
         msg = "Form included"
--- backend/utils/files.py
+++ backend/utils/files.py
@@ -3,7 +3,7 @@
 """
 
 def check_file_extension(filename: str):
-    allowed_extensions = {'txt', 'pdf'}
+    allowed_extensions = {'txt', 'eml', 'pdf'}
     # Check if there is a period in the filename and splits the extension at the last period. 
     # Then convert it to lowercase and checks if it is allowed
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_extensions
